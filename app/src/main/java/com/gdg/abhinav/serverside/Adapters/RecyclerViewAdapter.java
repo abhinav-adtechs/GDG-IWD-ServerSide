cdd63c8c93d2fac529160999e457de066c8d8ffd
abhinav-adtechs <abhinav.adtechs@gmail.com>
2016-03-07 12:44:56 +0530

    Initial commit

---
+++ app/src/main/java/com/gdg/abhinav/serverside/Adapters/RecyclerViewAdapter.java
@@ -0,0 +1,7 @@
+package com.gdg.abhinav.serverside.Adapters;
+
+/**
+ * Created by jordiie-admin on 5/3/16.
+ */
+public class RecyclerViewAdapter {
+}
---
+++ app/src/main/java/com/gdg/abhinav/serverside/Utilities/Constants.java
@@ -0,0 +1,7 @@
+package com.gdg.abhinav.serverside.Utilities;
+
+/**
+ * Created by jordiie-admin on 4/3/16.
+ */
+public class Constants {
+}
